bddd6672d6596c1850f87ddec727a861740dcdc5
Nikolai Smirnov <dewhitemold@yandex.ru>
2023-04-15 16:22:53 +0200

    connect to mongo changed

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.use(requestLogger);
 
